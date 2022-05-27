f1e8b37bea19830f5e8ca8c4a60fcceb135ad7e2
Chelsea Jo Calo <chelseajocalo@gmail.com>
2022-05-27 11:44:45 -0400

    submit iteration 1

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,106 @@
+package virtual_pet;
+
+public class VirtualPet {
+
+    private String name;
+    private int thirstLevel;
+    private int hungerLevel;
+    private int boredomLevel;
+
+    public VirtualPet(String name, int thirstLevel, int hungerLevel, int boredomLevel) {
+        this.name = name;
+        this.thirstLevel = thirstLevel;
+        this.hungerLevel = hungerLevel;
+        this.boredomLevel = boredomLevel;
+    }
+// put three 3 methods
+    public boolean tooHungry() {
+        if(getHungerLevel()>=5) {
+            return true;
+        }
+        return false;
+    }
+    public void play(){
+//    if(getHungerLevel()>=5){
+//        System.out.println("I am not playing until I get a snack.");
+//    }
+//    else if(getHungerLevel()<5) {
+//        boredomLevel -= 2;
+//        System.out.println("Thanks for playing with me.");
+//    }
+
+        System.out.println("Thanks for playing with me.");
+    }
+    public boolean tooBored() {
+        if(getBoredomLevel()>=4) {
+            System.out.println("I'm too bored. Play with me.");
+            return true;
+        }
+        return false;
+    }
+    public boolean isAlive() {
+        if(getThirstLevel() > 8) {
+            return false;
+        }
+        else if(getHungerLevel() > 10) {
+            return false;
+        }
+        else if(getBoredomLevel() > 15) {
+            return false;
+        }
+        return true;
+    }
+    public void greeting(){
+        System.out.println("Hello! My name is " + name + ". Please take care of me!");
+    }
+    public void feed() {
+        hungerLevel -= 2;
+        System.out.println("Thanks for the snack.");
+    }
+    public void giveDrink() {
+        thirstLevel -= 2;
+        System.out.println("Thanks for the drink.");
+    }
+    //tick method
+    public void tick() {
+        thirstLevel += 1;
+        hungerLevel += 1;
+        boredomLevel += 1;
+        if(getHungerLevel()>=7) {
+            System.out.println("Your pet ate your shoes. Try to feed him sooner next time.");
+            hungerLevel = hungerLevel - 3;
+        }
+    }
+
+    //getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public int getBoredomLevel() {
+        return boredomLevel;
+    }
+    public void quitGame() {
+        System.out.println("Thanks for playing! Goodbye!");
+    }
+
+    public void status() {
+        System.out.print("Boredom Level = " + getBoredomLevel() + ".");
+        System.out.print(" Hunger Level = " + getHungerLevel() + ".");
+        System.out.println(" Thirst Level = " + getThirstLevel() + ".");
+    }
+
+    public void tickNoPlay() {
+        thirstLevel += 1;
+        hungerLevel += 1;
+
+    }
+}
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,107 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
+
+        VirtualPetApplication myGame = new VirtualPetApplication();
+        myGame.gameLoop();
         //Interact with a VirtualPet object in this method
+
     }
 
-}
+    public void gameLoop() {
+        Scanner input = new Scanner(System.in);
+        VirtualPet lollipop = new VirtualPet("Lollipop", 4, 5, 3);
+        lollipop.greeting();
+
+
+        do  {
+            lollipop.status();
+            System.out.println("What would you like to do next? Type feed, drink, or play. Press Q at any time to quit. Type status to see what I need.");
+            String task = input.nextLine();
+            if(task.equalsIgnoreCase("Q")) {
+                lollipop.quitGame();
+                break;
+            }
+            if (task.equals("feed")) {
+                lollipop.feed();
+            }
+            if (task.equals("drink")) {
+                lollipop.giveDrink();
+            }
+            if (task.equals("play")) {
+                lollipop.play();
+            }
+            if(lollipop.tooBored()) {
+                String bored = input.nextLine();
+                if (bored.equals("play")) {
+                    lollipop.tick();
+                    lollipop.play();
+                    continue;
+                }
+                else if (!bored.equals("play")) {
+                    System.out.println("No. I want to play.");
+                    lollipop.tickNoPlay();
+                    continue;
+
+                }
+                break;
+                }
+
+            lollipop.tick();
+        }
+        while(lollipop.isAlive());
+        if(!lollipop.isAlive()) {
+            System.out.println("Your pet is dead.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+//            String nextTask = input.nextLine();
+//            while(lollipop.isAlive()) {
+//                if(nextTask.equals("play")){
+//                    lollipop.play();
+//                    System.out.println("Thanks for playing.");
+//                }
+//                if(nextTask.equals("feed")) {
+//                    lollipop.feed();
+//                    System.out.println("Thanks for feeding me.");
+//                }
+//                if(nextTask.equals("drink")) {
+//                    lollipop.giveDrink();
+//                    System.out.println("Thanks for feeding me.");
+//                }
+//                if(lollipop.tooHungry()){
+//                    System.out.println("I'm too hungry. Please feed me now.");
+//                    String hungry = input.nextLine();
+//                        if(!hungry.equals("feed")) {
+//                            System.out.println("No. I asked for food.");
+//                        }
+//                        if(hungry.equals("feed")) {
+//                            System.out.println("That's much better, thank you!");
+//                            break;
+//                        }
+//                }
+//            }
+
+
+
+        }
+
+
+
+
+    }
+
